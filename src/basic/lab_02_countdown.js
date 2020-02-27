356553ae17a0f3774054d48f3176ca4e7962c539
Kamil Berdychowski <k.berdychowski@gmail.com>
2020-02-27 15:00:39 +0100

    Lab: Countdown

---
+++ src/basic/lab_02_countdown.js
@@ -0,0 +1,27 @@
+import { interval } from 'rxjs';
+import { scan, filter, mapTo, takeWhile, tap } from 'rxjs/operators';
+
+console.clear();
+
+const countdownElement = document.getElementById('countdown');
+const messageElement = document.getElementById('message');
+const countdownFrom = 10;
+
+console.log(countdownElement.innerHTML);
+countdownElement.innerHTML = countdownFrom;
+
+const counter$ = interval(300);
+counter$.pipe(
+    // each value will be changed to -1
+    mapTo(-1),
+    // scan is used to accumulate values over time
+    scan((accumulator, current) => accumulator + current, countdownFrom),
+    //filter will not stop subscription, take while will
+    // filter(v => v >= 0)
+    takeWhile(v => v >= 0)
+).subscribe((value) => {
+    countdownElement.innerHTML = value;
+    if (value == 0) {
+        messageElement.innerHTML = 'Liftoff!';
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: {
         'index': './src/index.js',
         '01_observables_and_observers': './src/basic/01_observables_and_observers.js',
-        'lab_01_scroll_progress_bar': './src/basic/lab_01_scroll_progress_bar.js'
+        'lab_01_scroll_progress_bar': './src/basic/lab_01_scroll_progress_bar.js',
+        'lab_02_countdown': './src/basic/lab_02_countdown.js'
     },
     plugins: [
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
