3edc00bc4c91fda6ac7c5140cc2df0c629fcd266
Kamil Berdychowski <k.berdychowski@gmail.com>
2020-02-27 20:43:56 +0100

    Added example for distinctUntilChanged and distinctUntilKeyChanged

--- src/basic/02_operators.js
+++ src/basic/02_operators.js
@@ -1,5 +1,5 @@
-import { range, fromEvent, interval } from "rxjs";
-import { take, map, filter, first, takeWhile, takeUntil, mapTo, scan } from "rxjs/operators";
+import { range, fromEvent, interval, from } from "rxjs";
+import { take, map, filter, first, takeWhile, takeUntil, mapTo, scan, distinctUntilChanged, distinctUntilKeyChanged } from "rxjs/operators";
 import { MyObserver } from '../common'
 
 console.clear();
@@ -9,16 +9,16 @@ console.clear();
 // ------------------------------------------------
 
 // take - takes specified number of elements and completes
-range(1,5).pipe(take(3)).subscribe(new MyObserver('Take'));
+range(1, 5).pipe(take(3)).subscribe(new MyObserver('Take'));
 
 //Like using filter and take(1)
 const clicks$ = fromEvent(document.getElementById('generator'), 'click');
 
 clicks$.pipe(
-    map(e => ({x: event.clientX, y: event.clientY})),
-    first(({y}) => y > 260)
+    map(e => ({ x: event.clientX, y: event.clientY })),
+    first(({ y }) => y > 260)
 )
-.subscribe(new MyObserver('first'));
+    .subscribe(new MyObserver('first'));
 
 // ------------------------------------------------
 // Complete a stream when condition is met
@@ -27,10 +27,10 @@ clicks$.pipe(
 // takeWhile emits values as long as they pass condition
 // second parameter makes takeWhile to emit value that stopped stream (true) or not (false)
 clicks$.pipe(
-    map(e => ({x: event.clientX, y: event.clientY})),
-    takeWhile(({y}) => y <= 260, true)
+    map(e => ({ x: event.clientX, y: event.clientY })),
+    takeWhile(({ y }) => y <= 260, true)
 )
-.subscribe(new MyObserver('takeWhile'));
+    .subscribe(new MyObserver('takeWhile'));
 
 // ------------------------------------------------
 // Complete a stream based on other stream
@@ -58,3 +58,29 @@ counter$.pipe(
         messageElement.innerHTML = 'Liftoff!';
     }
 });
+
+// ------------------------------------------------
+// Ignore non unique values
+// ------------------------------------------------
+
+const numbers = [1, '1', 2, 3, 4, 5, 5, 6, 6, 7, 8, 9, 9, 9, 10];
+
+from(numbers).pipe(
+    // distinctUntilChanged uses === comparison by default, object references must match!
+    distinctUntilChanged()
+).subscribe(new MyObserver('distinctUntilChanged'));
+
+from(numbers).pipe(
+    // you can provide custom comparator
+    distinctUntilChanged((prev, curr) => prev == curr)
+).subscribe(new MyObserver('distinctUntilChanged+custom'));
+
+from([
+    { name: 'Brian' },
+    { name: 'Joe' },
+    { name: 'Joe' },
+    { name: 'Sue' }
+]).pipe(
+    // usefull when comparing key from object
+    distinctUntilKeyChanged('name')
+).subscribe(new MyObserver('distinctUntilKeyChanged'));
